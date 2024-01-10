647ebd8fccb3ef619c1771ed9b507cbe917c606b
manhtu227 <manhtu2272002@gmail.com>
2024-01-10 23:34:52 +0700

    fix bug

--- Back_End/src/routes/index.routes.ts
+++ Back_End/src/routes/index.routes.ts
@@ -1,18 +1,18 @@
-import { Router } from 'express';
-import usersRouter from './users.routes';
-import boardsRouter from './board.routes';
-import reportsRouter from './report.routes';
-import locationsRoute from './location.routes';
-import adminRouter from './admin.routes';
-import licenseRouter from './license.routes';
+import { Router } from 'express'
+import usersRouter from './users.routes'
+import boardsRouter from './board.routes'
+import reportsRouter from './report.routes'
+import locationsRoute from './location.routes'
+import adminRouter from './admin.routes'
+import licenseRouter from './license.routes'
 
-const indexRouter = Router();
+const indexRouter = Router()
 
-indexRouter.use('/users', usersRouter);
-indexRouter.use('/admins', adminRouter);
-indexRouter.use('/boards', boardsRouter);
-indexRouter.use('/reports', reportsRouter);
-indexRouter.use('/locations', locationsRoute);
-indexRouter.use('/licenses', licenseRouter);
+indexRouter.use('/users', usersRouter)
+indexRouter.use('/admins', adminRouter)
+indexRouter.use('/boards', boardsRouter)
+indexRouter.use('/reports', reportsRouter)
+indexRouter.use('/locations', locationsRoute)
+indexRouter.use('/licenses', licenseRouter)
 
-export default indexRouter;
+export default indexRouter
--- Back_End/src/routes/report.routes.ts
+++ Back_End/src/routes/report.routes.ts
@@ -1,16 +1,21 @@
-import { Router } from 'express';
-import { wrapRequestHandler } from '../utils/handler.ultil';
-import { createReport, getReportAnonymousByConditionController, getReportByConditionController, getReportByIdController, updateReportController } from '../controllers/reports.controller';
-import { createReportValidate } from '../middlewares/report.middlewares';
-import multer from 'multer';
-import { accessTokenValidator } from '../middlewares/users.middlewares';
-import { getBoardByIdController } from '../controllers/boards.controllers';
+import { Router } from 'express'
+import { wrapRequestHandler } from '../utils/handler.ultil'
+import {
+  createReport,
+  getReportAnonymousByConditionController,
+  getReportByConditionController,
+  getReportByIdController,
+  updateReportController
+} from '../controllers/reports.controller'
+import { createReportValidate } from '../middlewares/report.middlewares'
+import multer from 'multer'
+import { accessTokenValidator } from '../middlewares/users.middlewares'
+import { getBoardByIdController } from '../controllers/boards.controllers'
 
-const reportsRouter = Router();
+const reportsRouter = Router()
 
 // anonymous
-reportsRouter.get('/:id', wrapRequestHandler(getReportByIdController));
-reportsRouter.get('/anonymous', wrapRequestHandler(getReportAnonymousByConditionController));
+reportsRouter.get('/anonymous', wrapRequestHandler(getReportAnonymousByConditionController))
 
 const upload = multer({
   storage: multer.memoryStorage(),
@@ -18,11 +23,12 @@ const upload = multer({
     fileSize: 10 * 1024 * 1024, // 2 MB
     files: 1
   }
-});
-reportsRouter.post('/anonymous', upload.single('file'), createReportValidate, wrapRequestHandler(createReport));
+})
+reportsRouter.post('/anonymous', upload.single('file'), createReportValidate, wrapRequestHandler(createReport))
 
 //officer
-reportsRouter.get('/officer', accessTokenValidator, wrapRequestHandler(getReportByConditionController));
-reportsRouter.patch('/officer', accessTokenValidator, wrapRequestHandler(updateReportController));
+reportsRouter.get('/officer', accessTokenValidator, wrapRequestHandler(getReportByConditionController))
+reportsRouter.patch('/officer', accessTokenValidator, wrapRequestHandler(updateReportController))
+reportsRouter.get('/:id', wrapRequestHandler(getReportByIdController))
 
-export default reportsRouter;
+export default reportsRouter
