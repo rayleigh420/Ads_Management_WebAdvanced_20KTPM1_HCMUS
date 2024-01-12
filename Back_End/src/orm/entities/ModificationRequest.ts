61c3e5c3c4ed74f805177d1399ca71da5aab701a
Daniel <daniel.c@itcgroup.io>
2024-01-12 23:16:02 +0700

    update modification request entity

--- Back_End/src/orm/entities/ModificationRequest.ts
+++ Back_End/src/orm/entities/ModificationRequest.ts
@@ -1,58 +1,61 @@
-import { Column, Entity, Index, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm'
-import { AdvertisingBoard } from './AdvertisingBoard'
-import { AdvertisingLocation } from './AdvertisingLocation'
+import { Column, Entity, Index, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { AdvertisingBoard } from './AdvertisingBoard';
+import { AdvertisingLocation } from './AdvertisingLocation';
 
 @Index('modification_request_FK', ['boardId'], {})
 @Index('modification_request_FK_1', ['newLocationId'], {})
 @Entity('modification_request', { schema: 'ads_management' })
 export class ModificationRequest {
   @PrimaryGeneratedColumn({ type: 'int', name: 'id', unsigned: true })
-  id: number
+  id: number;
 
   @Column('int', { name: 'new_location_id', unsigned: true, nullable: true })
-  newLocationId: number | null
+  newLocationId: number | null;
 
   @Column('datetime', { name: 'request_time' })
-  requestTime: Date
+  requestTime: Date;
 
   @Column('text', { name: 'reason' })
-  reason: string
+  reason: string;
 
   @Column('int', { name: 'officer_id', nullable: true })
-  officerId: number | null
+  officerId: number | null;
 
   @Column('int', { name: 'board_type', nullable: true })
-  boardType: number | null
+  boardType: number | null;
+
+  @Column('int', { name: 'status', nullable: true, default: 0 })
+  status: number | null;
 
   @Column('float', { name: 'width', nullable: true, precision: 12 })
-  width: number | null
+  width: number | null;
 
   @Column('float', { name: 'height', nullable: true, precision: 12 })
-  height: number | null
+  height: number | null;
 
   @Column('varchar', { name: 'lat', nullable: true, length: 20 })
-  lat: string | null
+  lat: string | null;
 
   @Column('varchar', { name: 'long', nullable: true, length: 20 })
-  long: string | null
+  long: string | null;
 
   @Column('varchar', { name: 'address', nullable: true, length: 100 })
-  address: string | null
+  address: string | null;
 
   @Column('int', { name: 'board_id', nullable: true, unsigned: true })
-  boardId: number | null
+  boardId: number | null;
 
   @ManyToOne(() => AdvertisingBoard, (advertisingBoard) => advertisingBoard.modificationRequests, {
     onDelete: 'NO ACTION',
-    onUpdate: 'NO ACTION'
+    onUpdate: 'NO ACTION',
   })
   @JoinColumn([{ name: 'board_id', referencedColumnName: 'id' }])
-  board: AdvertisingBoard
+  board: AdvertisingBoard;
 
   @ManyToOne(() => AdvertisingLocation, (advertisingLocation) => advertisingLocation.modificationRequests, {
     onDelete: 'NO ACTION',
-    onUpdate: 'NO ACTION'
+    onUpdate: 'NO ACTION',
   })
   @JoinColumn([{ name: 'new_location_id', referencedColumnName: 'id' }])
-  newLocation: AdvertisingLocation
+  newLocation: AdvertisingLocation;
 }
